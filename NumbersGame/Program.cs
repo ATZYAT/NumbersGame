bcb1a10c125da318e620f3ee52c1188ee9cd384a
ATZYAT <ahmet.tokmak@student.chasacademy.se>
2026-09-26 00:45:52 +0200

    Initial commit

---
+++ NumbersGame/Program.cs
@@ -0,0 +1,44 @@
+﻿
+ 
+ Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");    
+    
+
+    Random random = new Random();
+
+    int number = random.Next(1,20);
+    
+     bool isCorrect = false;
+
+    for (int i = 0; i < 5; i++)
+    
+{
+    int guess = int.Parse(Console.ReadLine());
+
+    if (guess > number)
+
+{
+    Console.WriteLine("Tyvärr, du gissade för högt!");
+}
+else if (guess < number)
+
+{
+    Console.WriteLine("Tyvärr, du gissade för lågt!");
+}
+
+else
+
+    {
+        Console.WriteLine("Wohoo! Du klarade det!");
+    
+        isCorrect = true;
+
+        break;
+    } 
+
+}
+ 
+    if (!isCorrect)
+  {
+        Console.WriteLine("Tyvärr, du lyckades inte gissa talet på fem försök!");
+    }
+
